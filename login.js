cc2c3942c09ab2aa026ca5b36ec590b07fea16f3
KyllianGenot <kygenot@gmail.com>
2025-02-07 13:56:19 +0100

    Initial commit

---
+++ login.js
@@ -0,0 +1,26 @@
+const usernameEl = document.querySelector("#username")
+const passwordEl = document.querySelector("#password")
+
+export function logIn(event) {
+    event.preventDefault()
+    const username = usernameEl.value
+    const password = passwordEl.value
+
+    const data = {
+        username: username,
+        password: password,
+    }
+    fetch("/api/user", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+    }).then((res) => {
+        return res.text()
+    }).then((text) => {
+        console.log(text)
+    }).catch(function (error) {
+        console.log("Promise rejected with:", error)
+    })
+}
