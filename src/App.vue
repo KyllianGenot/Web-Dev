9706574631a571eb0f53a1bf8c7b0af66f80e907
KyllianGenot <kygenot@gmail.com>
2025-03-29 10:35:05 +0100

    feat: :sparkles: set up Vue app with Vite and TypeScript

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,38 @@
 <script lang="ts" setup>
-import { RouterView } from 'vue-router';
-
+import { RouterLink, RouterView } from 'vue-router';
 import AppHeader from './components/AppHeader.vue';
+
+const isAuthenticated = !!localStorage.getItem('token');
 </script>
 
 <template>
-  <AppHeader />
-  <main>
-    <header>
-      <RouterLink to="/signin">Sign in</RouterLink>
-      <RouterLink to="/signup">Sign up</RouterLink>
-    </header>
-    <RouterView />
-  </main>
+  <div>
+    <AppHeader />
+    <main class="container mx-auto p-4">
+      <nav class="mb-4">
+        <RouterLink
+          v-if="!isAuthenticated"
+          to="/signin"
+          class="text-blue-600 hover:underline mr-4"
+        >
+          Sign In
+        </RouterLink>
+        <RouterLink
+          v-if="!isAuthenticated"
+          to="/signup"
+          class="text-blue-600 hover:underline mr-4"
+        >
+          Sign Up
+        </RouterLink>
+        <RouterLink
+          v-if="isAuthenticated"
+          to="/todos"
+          class="text-blue-600 hover:underline"
+        >
+          Todos
+        </RouterLink>
+      </nav>
+      <RouterView />
+    </main>
+  </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,66 +1,51 @@
-import { signUp } from "./signup.js"
-import { signIn } from "./signin.js"
 import SignUp from './components/SignUp.vue';
 import SignIn from './components/SignIn.vue';
-import App from './App.vue'
+import Todos from './components/Todos.vue';
+import App from './App.vue';
 
-import { createRouter, createWebHistory } from "vue-router"
-import { ref, computed, createApp } from 'vue'
-import 'uno.css'
-import '@una-ui/preset/una.css'
+import { createRouter, createWebHistory } from 'vue-router';
+import { createApp } from 'vue';
+import 'uno.css';
+import '@una-ui/preset/una.css';
 
 const routes = [
   {
     name: 'Home',
     path: '/',
-    redirect: '/signin'
+    redirect: '/signin',
   },
   {
     name: 'SignIn',
     path: '/signin',
-    component: SignIn
+    component: SignIn,
   },
   {
     name: 'SignUp',
     path: '/signup',
-    component: SignUp
+    component: SignUp,
   },
-]
+  {
+    name: 'Todos',
+    path: '/todos',
+    component: Todos,
+    meta: { requiresAuth: true },
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
-})
+});
+
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem('token');
+  if (to.meta.requiresAuth && !token) {
+    next('/signin');
+  } else {
+    next();
+  }
+});
 
 createApp(App)
   .use(router)
-  .mount('#app')
-
-// const radioButtons = document.querySelectorAll('[name="formType"]')
-
-// function displayAppropriateForm (event) {
-//   if (event.target.checked) {
-//     if (event.target.value === 'signin') {
-//       signupFormEl.style.display = 'none'
-//       signinFormEl.style.display = ''
-//       return
-//     }
-//     signinFormEl.style.display = 'none'
-//     signupFormEl.style.display = ''
-//   }
-// }
-
-// for (const radioButton of radioButtons) {
-//   radioButton.addEventListener('change', displayAppropriateForm)
-// }
-
-// const signupFormEl = document.querySelector('#signup')
-// const signinFormEl = document.querySelector('#signin')
-
-// if (signupFormEl) {
-//   signupFormEl.addEventListener('submit', signUp)
-// }
-
-// if (signinFormEl) {
-//   signinFormEl.addEventListener('submit', signIn)
-// }
+  .mount('#app');
