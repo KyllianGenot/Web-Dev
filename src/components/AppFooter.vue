6d7de95ba97d4ab048051ed26ff5cd54f0fc1e08
KyllianGenot <kygenot@gmail.com>
2025-03-29 15:35:08 +0100

    feat: 🎨 update app name to Taskify in footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -4,7 +4,7 @@ const currentYear = new Date().getFullYear();
 
 <template>
   <footer class="bg-white border-t border-gray-200 py-6 text-center text-gray-500 text-sm mt-12">
-    <p>© {{ currentYear }} Todo App. All rights reserved.</p>
+    <p>© {{ currentYear }} Taskify. All rights reserved.</p>
     <p class="mt-1">
       Built by Kyllian |
       <a
